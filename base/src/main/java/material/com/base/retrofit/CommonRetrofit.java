2c3e1478cdd82dad215450cd62d7218da8f25632
cangwang <284699931@qq.com>
2017-03-28 18:37:47 +0800

    [3.28]add Retrofit

--- base/src/main/java/material/com/base/retrofit/CommonRetrofit.java
+++ base/src/main/java/material/com/base/retrofit/CommonRetrofit.java
@@ -1,10 +1,11 @@
 package material.com.base.retrofit;
 
+import com.jakewharton.retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
+
 import java.util.concurrent.TimeUnit;
 
 import okhttp3.OkHttpClient;
 import retrofit2.Retrofit;
-//import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 /**
@@ -33,7 +34,7 @@ public class CommonRetrofit {
         retrofit=new Retrofit.Builder()
                 .client(builder.build())
                 .addConverterFactory(GsonConverterFactory.create())
-//                .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
                 .baseUrl(BASE_URL)
                 .build();
     }
